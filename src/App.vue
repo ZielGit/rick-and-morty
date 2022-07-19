08d6e600fec21d532bc3d070595c60cdab770876
Ziel <frans_421_12@hotmail.com>
2022-07-18 20:24:17 -0500

    feat: Mostrando los datos de la api

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
     <h1>Rick And Morty</h1>
+    <listCharacters />
   </div>
 </template>
 
 <script>
+import ListCharacters from './components/ListCharacters.vue'
 
 export default {
   name: 'App',
   components: {
+    ListCharacters
     
   }
 }
